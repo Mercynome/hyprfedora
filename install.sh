dea1857fc428518e518c0fd8687b92b3f6806978
Mercynome <mercynome@proton.me>
2026-06-14 19:52:01 +0300

    fix: Add qt6-qt5compat for Quickshell GraphicalEffects compatibility

--- install.sh
+++ install.sh
@@ -27,14 +27,14 @@ if [ "$OS" == "fedora" ]; then
     sudo dnf copr enable -y solopasha/hyprland
     sudo dnf copr enable -y scottames/awww
     sudo dnf copr enable -y sdegler/hyprland
-    sudo dnf install -y --skip-unavailable kitty fuzzel waybar SwayNotificationCenter wlogout kvantum qt5ct qt6ct cliphist satty waypaper pyprland hyprland hyprpolkitagent hypridle hyprlock hyprsunset hyprshot nwg-look awww cargo rust-packaging gtk4-layer-shell-devel wget tar xz zsh util-linux-user unzip cava btop rofi nwg-drawer swww quickshell qt6-qtwayland qt6-qtsvg qt6-qtdeclarative
+    sudo dnf install -y --skip-unavailable kitty fuzzel waybar SwayNotificationCenter wlogout kvantum qt5ct qt6ct cliphist satty waypaper pyprland hyprland hyprpolkitagent hypridle hyprlock hyprsunset hyprshot nwg-look awww cargo rust-packaging gtk4-layer-shell-devel wget tar xz zsh util-linux-user unzip cava btop rofi nwg-drawer swww quickshell qt6-qtwayland qt6-qtsvg qt6-qtdeclarative qt6-qt5compat
     
     echo "hyprKCS (Arayüzlü Tuş Yöneticisi) Cargo ile kuruluyor..."
     cargo install hyprKCS
 
 elif [ "$OS" == "arch" ]; then
     echo "Arch Linux tespit edildi! Paketler pacman/paru ile kuruluyor..."
-    sudo pacman -Syu --needed hyprland kitty fuzzel waybar wlogout kvantum qt5ct qt6ct cliphist pyprland hypridle hyprlock cargo wget tar xz zsh unzip cava btop rofi swww qt6-wayland qt6-declarative qt6-svg
+    sudo pacman -Syu --needed hyprland kitty fuzzel waybar wlogout kvantum qt5ct qt6ct cliphist pyprland hypridle hyprlock cargo wget tar xz zsh unzip cava btop rofi swww qt6-wayland qt6-declarative qt6-svg qt6-5compat
     # AUR paketleri için yay veya paru varsayıyoruz:
     if command -v paru &> /dev/null; then
         paru -S --needed swaync satty waypaper hyprpolkitagent hyprsunset hyprshot nwg-look awww-git hyprkcs-git nwg-drawer quickshell
